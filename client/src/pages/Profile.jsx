eed43db3710de7f03a199502ac015afa1328697b
5arimNadeem <sarimnadeem141@gmail.com>
2026-03-02 15:05:16 +0500

    complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,6 +17,8 @@ const Profile = () => {
   const [fileUploadError, setFileUploadError] = useState(false)
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false)
+  const [showListingError, setShowListingError] = useState(false)
+  const [userListings, setUserListings] = useState([])
   const dispatch = useDispatch()
   // const [interval, setInterval] = useState(null);
 
@@ -117,6 +119,21 @@ const Profile = () => {
       dispatch(deleteUserFailure(data.message))
     }
   }
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingError(false)
+      const res = await fetch(`/api/user/listings/${currentUser._id}`)
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingError(true)
+        return
+      }
+      setUserListings(data)
+    } catch (error) {
+      showListingError(true)
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -198,7 +215,36 @@ const Profile = () => {
 
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='green-red-700 mt-5'>{updateSuccess ? "User is updated successfully" : ''}</p>
+      <button
+        onClick={handleShowListings}
+        className='text-green-700 w-full'>Show listing</button>
+      <p className='text-red-700 mt-5'>{showListingError ? 'error showing Listings' : ''}</p>
 
+      {userListings && userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+          {userListings.map((listing) => (
+            <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+              <Link to={`/listing/${listing._id}`}>
+                <img src={listing.imageUrls[0]} alt="listingCover" className='h-16 w-26 object-contain' />
+              </Link>
+              <Link className='text-slate-700 font-semibold  hover:underline truncate flex-1' to={`/listing/${listing._id}`}>
+                {/* truncate : when the title is too long what it does is too make the title shorter by adding ... at the end of the title */}
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className='flex flex-col items-center'>
+                <button className='text-red-700 uppercase'>
+                  Delete
+                </button>
+                <button
+                  // onClick={handleShowListings}
+                  className='text-green-700 w-full'>Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      }
     </div>
   )
 }
